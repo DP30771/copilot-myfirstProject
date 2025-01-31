ea52e7027b0b2d125bfdee86ca848428d9000c39
DP30771 <dpeddi@pekininsurance.com>
2025-01-31 20:02:00 +0000

    my second commit

---
+++ member.js
@@ -0,0 +1,116 @@
+function skillsMember() {
+  return [
+    {
+      name: "HTML",
+      level: "Intermediate",
+    },
+    {
+      name: "CSS",
+      level: "Beginner",
+    },
+    {
+      name: "JavaScript",
+      level: "Intermediate",
+    },
+    {
+      name: "React",
+      level: "Intermediate",
+    },
+    {
+      name: "Node.js",
+      level: "Beginner",
+    },
+    {
+      name: "MongoDB",
+      level: "Beginner",
+    },
+    {
+      name: "PostgreSQL",
+      level: "Beginner",
+    },
+    {
+      name: "Python",
+      level: "Beginner",
+    },
+    {
+      name: "Django",
+      level: "Beginner",
+    },
+    {
+      name: "Ruby",
+      level: "Beginner",
+    },
+    {
+      name: "Ruby on Rails",
+      level: "Beginner",
+    },
+    {
+      name: "Git",
+      level: "Intermediate",
+    },
+    {
+      name: "SQL",
+      level: "Intermediate",
+    },
+    {
+      name: "C",
+      level: "Beginner",
+    },
+    {
+      name: "C++",
+      level: "Beginner",
+    },
+    {
+      name: "Java",
+      level: "Beginner",
+    },
+    {
+      name: "Android",
+      level: "Beginner",
+    },
+    {
+      name: "Swift",
+      level: "Beginner",
+    },
+    {
+      name: "Kotlin",
+      level: "Beginner",
+    },
+    {
+      name: "Firebase",
+      level: "Beginner",
+    },
+    {
+      name: "AWS",
+      level: "Beginner",
+    },
+    {
+      name: "Heroku",
+      level: "Beginner",
+    },
+    {
+      name: "Netlify",
+      level: "Beginner",
+    },
+    {
+      name: "Vercel",
+      level: "Beginner",
+    },
+    {
+      name: "Docker",
+      level: "Beginner",
+    },
+    {
+      name: "Kubernetes",
+      level: "Beginner",
+    },
+    {
+      name: "Jenkins",
+      level: "Beginner",
+    },
+    {
+      name: "Travis CI",
+      level: "Beginner",
+    },
+    {
+      name: "
